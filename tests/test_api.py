3b5bcef5d22593a504216bb67ad24db31d4bbcc7
Ryan Ricardo de Souza <140509330+theunrealryan@users.noreply.github.com>
2025-08-20 15:36:18 -0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,17 +10,7 @@ def client(app):
     """Um cliente de teste para a aplicação."""
     return app.test_client()
 
-def test_hello_world(cimport pytest
-from app import app as flask_app
-
-@pytest.fixture
-def app():
-    yield flask_app
-
-@pytest.fixture
-def client(app):
-    """Um cliente de teste para a aplicação."""
-    return app.test_clientlient):
+def test_hello_world(client):
     """Testa o endpoint principal '/'."""
     response = client.get('/')
     assert response.status_code == 200
